9c5f28d02e8da71cbb25f1c8aaa56902e57386b3
CarlosNaro <elmaximolider75@gmail.com>
2023-09-28 18:15:34 -0500

    arreglo de la vista user

--- Apps/user/viewSets.py
+++ Apps/user/viewSets.py
@@ -1,6 +1,6 @@
 # segundo paso para la creación de un api user
 from .models import User
-from .serializer import UserSerializer, MyTokenObtainPairSerializer
+from .serializer import UserSerializer
 from rest_framework.viewsets import ModelViewSet
 
 
@@ -10,6 +10,3 @@ from rest_framework.viewsets import ModelViewSet
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-#  creación de un api Login con JWT que devuelve un token y el usuario logueado
-
